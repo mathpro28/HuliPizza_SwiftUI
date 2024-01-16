326cecd7c54bccd7803c6fc9ef7e98588f1678e9
mathpro28 <matemerc@gmail.com>
2024-01-15 21:33:05 -0400

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Mateo Mercado Maguiña on 15/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Mateo Mercado Maguiña on 15/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
