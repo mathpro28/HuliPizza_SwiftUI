c72e13113454cffcd400d06a5f64290f61ed2f68
mathpro28 <matemerc@gmail.com>
2024-01-18 09:34:56 -0400

    Added view that is in charge of displaying pizza's description, price and rating.

--- HuliPizza/MenuRowView.swift
+++ HuliPizza/MenuRowView.swift
@@ -8,20 +8,30 @@
 import SwiftUI
 
 struct MenuRowView: View {
-    var item: Int
+    var item: MenuItem
     var body: some View {
         HStack(alignment: .top, spacing: 15) {
-            if let image = UIImage(named: "\(item)_sm") {
+            if let image = UIImage(named: "\(item.id)_sm") {
                 Image(uiImage: image)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, -25)
+                    .padding(.leading, -5)
+            } else {
+                Image("surfboard_sm")
             }
             VStack(alignment: .leading) {
-                Text("Margharita")
-                RatingsView(rating: 5)
+                HStack {
+                    Text(item.name)
+                    Spacer()
+                    Text(item.price, format: .currency(code: "USD"))
+                }
+                RatingsView(rating: item.rating)
             }
+            Spacer()
         }
     }
 }
 
 #Preview {
-    MenuRowView(item: 6)
+    MenuRowView(item: testMenuItem)
 }
