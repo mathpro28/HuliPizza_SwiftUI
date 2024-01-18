e74fa817ff3d32d871e6dbd53a7f87323934a4bf
mathpro28 <matemerc@gmail.com>
2024-01-18 09:28:33 -0400

    Added screen that displays the OrderView. Screen that shows all of the orders and their prices.

--- HuliPizza/OrderView.swift
+++ HuliPizza/OrderView.swift
@@ -8,30 +8,48 @@
 import SwiftUI
 
 struct OrderView: View {
-    var orders:[Int]
+    @Binding var orders:[OrderItem]
     var body: some View {
         VStack {
-            
-        
-            Label {
-                Text(59.99, format: .currency(code: "USD"))
-            }
-        icon: {
-            Image(systemName: orders.isEmpty ? "cart": "cart.circle.fill")
-        }
-            HStack {
-                Text("Order Pizza")
-                    .font(.title)
-                Spacer()
-            }
-            ScrollView() {
-                ForEach(orders, id: \.self) { order in
-                    OrderRowView(order: order)
+            ZStack(alignment: .top) {
+                ScrollView {
+                    ForEach($orders) { order in
+//                        Text(order.item.name)
+                        OrderRowView(order: order)
+                            .padding(4)
+                            .background(.regularMaterial, in:RoundedRectangle(cornerRadius: 10))
+                            .shadow(radius: 10)
+                            .padding(.bottom, 5)
+                            .padding([.leading, .trailing], 7)
+                    }
                 }
-        }}
+                .padding(.top, 80)
+                HStack {
+                    Text("Order Pizza")
+                        .font(.title)
+                    Spacer()
+                    Label {
+                        Text(59.99, format: .currency(code: "USD"))
+                    }
+                    icon: {
+                        Image(systemName: orders.isEmpty ? "cart": "cart.circle.fill")
+                    }
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+            }
+            .padding()
+            Button("Delete Order") {
+                if !orders.isEmpty{ orders.removeLast() }
+            }
+            .padding(5)
+            .background(.regularMaterial, in:Capsule())
+            .padding(7)
+        }
+        .background(Color("Surf"))
     }
 }
 
 #Preview {
-    OrderView(orders: [1,2,3,4])
+    OrderView(orders: .constant(testOrders))
 }
