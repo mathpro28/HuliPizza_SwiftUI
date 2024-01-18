9f75aeace07bde4568665a266e8bdae77f1a818f
mathpro28 <matemerc@gmail.com>
2024-01-18 09:16:15 -0400

    Added ContentView which contains references to other views, @State and @Binding.

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var orders:[Int] = [1,2,3,4]
+    var menu:[MenuItem]
+    @State private var orders:[OrderItem] = testOrders
+    @State private var showOrders:Bool = false
+    @State private var selectedItem:MenuItem = noMenuItem
     var body: some View {
         VStack {
             HeaderView()
-            OrderView(orders: orders)
-            MenuItemView()
-            MenuView()
+                .shadow(radius: 5)
+            HStack {
+                Text("\(orders.count) orders")
+                Spacer()
+                Button {
+                    showOrders.toggle()
+                } label: {
+                    Image(systemName: showOrders ? "cart" : "menucard")
+                        
+                }
+            }
+            .foregroundStyle(.white)
+            .font(.title2)
+            
+            if showOrders {
+                OrderView(orders: $orders)
+                    .cornerRadius(10)
+            } else {
+                MenuItemView(item: $selectedItem)
+                    .padding(5)
+                    .background(.thinMaterial, in: RoundedRectangle(cornerRadius: 10))
+                MenuView(menu:menu, selectedItem: $selectedItem)
+            }
+            Spacer()
+            
             
         }
         .padding()
+        .background(.linearGradient(colors: [.cyan, Color("Surf"), Color("Sky"), .white], startPoint: .topLeading, endPoint: .bottom))
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(menu:MenuModel().menu)
  }
