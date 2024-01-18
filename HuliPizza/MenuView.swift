a0853b264d64c702e55170532b0c6024e7cbdb6c
mathpro28 <matemerc@gmail.com>
2024-01-18 09:32:02 -0400

    Added view that is in charge of displaying all of the menu options and also each of the pizza's descriptions.

--- HuliPizza/MenuView.swift
+++ HuliPizza/MenuView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct MenuView: View {
+    var menu:[MenuItem]
+    @Binding var selectedItem:MenuItem
     var body: some View {
-        ScrollView() {
-            ForEach(1...10, id: \.self) { item in
-                MenuRowView(item: item)
-            }
-        }
+        List(MenuCategory.allCases, id:\.self) { category in
+            Section {
+                ForEach(menu.filter({$0.category == category})) { item in
+                    MenuRowView(item: item)
+                        .onTapGesture {
+                            selectedItem = item
+                        }
+                }
+            } header: {
+                Text(category.rawValue)
+            }        }
     }
 }
 
 #Preview {
-    MenuView()
+    MenuView(menu:MenuModel().menu, selectedItem: .constant(testMenuItem))
 }
