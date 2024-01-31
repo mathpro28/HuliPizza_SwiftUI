e47821205604ea8df52df5479d919874ff0534bd
mathpro28 <matemerc@gmail.com>
2024-01-31 18:54:32 -0400

    Added a picker for the pizzas.

--- HuliPizza/OrderItemView.swift
+++ HuliPizza/OrderItemView.swift
@@ -11,6 +11,7 @@ struct OrderItemView: View {
     @Binding var orderItem:OrderItem
     @State private var quantity = 1
     @State private var doubleIngredient = false
+    @State private var pizzaCrust:PizzaCrust = .calzone
     var body: some View {
         VStack {
             Toggle(isOn: $doubleIngredient) {
@@ -18,6 +19,14 @@ struct OrderItemView: View {
                 Stepper(value:$quantity, in:1...10) {
                     Text("\(quantity) " + (quantity == 1 ? "pizza" : "pizzas"))
                 }
+                Picker(selection: $pizzaCrust) { ForEach(PizzaCrust.allCases,id:\.self) { crust in
+                    Text(crust.rawValue).tag(crust)
+                    }
+                } label: {
+                    Text("Pizza Crust")
+                }
+                .pickerStyle(MenuPickerStyle())
+                Spacer()
             }
         }
     }
